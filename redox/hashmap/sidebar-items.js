5de6babd73cb2cf2fda4fdf3244dde2cacb8a16f
Ticki <Ticki@users.noreply.github.com>
2015-11-06 12:49:06 +0100

    Add files

---
+++ redox/hashmap/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["LinkedList","An linked list (used for entries)"]],"fn":[["test",""]],"struct":[["Djb2","DJB2 hashing"],["Entry","A entry in the hash map"],["HashMap","A hashtable"]]});
